c7164542caf9141b1df4c8e4e2942f2580cc2762
Bahram2006 <yardam034@gmail.com>
2026-05-06 09:49:39 +0500

    prisma dev problem eror not

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// 'export' sözi bu faýly modul edýär we beýleki ýerlerde ulanmaga rugsat berýär
 export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: "file:./dev.db", // Maglumat bazasynyň ýoly hökman şu taýda bolmaly
+    url: "file:./dev.db", // Maglumat bazasynyň ýoly şu taýda bolmaly
   },
 });
